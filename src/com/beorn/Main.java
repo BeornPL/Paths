79c7c5f484c6541ce57b349b2726ec464f12d8b4
BeornPL <37457935+BeornPL@users.noreply.github.com>
2018-06-08 18:58:49 +0200

    2nd video More on Path

--- src/com/beorn/Main.java
+++ src/com/beorn/Main.java
@@ -12,10 +12,18 @@ public class Main {
     public static void main(String[] args) {
         Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
         printFile(path);
-        Path filePath = FileSystems.getDefault().getPath("files", "SubdirectoryFile.txt");
+//        Path filePath = FileSystems.getDefault().getPath("files", "SubdirectoryFile.txt");
+        Path filePath = Paths.get(".", "files", "SubdirectoryFile.txt");
         printFile(filePath);
-        filePath = Paths.get("C:\\Users\\Beorn\\Desktop\\dev\\Java\\#Tim's\\Section 15 - Basic Input & Output\\OutThere.txt");
+        filePath = Paths.get("C:\\Users\\Beorn\\Desktop\\dev\\Java\\#Tim's", "\\Section 15 - Basic Input & Output", "OutThere.txt");
         printFile(filePath);
+
+        filePath = Paths.get(".");
+        System.out.println(filePath.toAbsolutePath());
+
+        Path path2 = FileSystems.getDefault().getPath(".", "files", "..", "files", "SubdirectoryFile.txt");
+        System.out.println(path2.normalize().toAbsolutePath());
+        printFile(path2.normalize());
 
     }
 
